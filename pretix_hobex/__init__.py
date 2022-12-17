66a1ad9316f5e0487580b32968492d00b45da1a0
Raphael Michel <michel@rami.io>
2022-12-17 17:59:32 +0100

    Stop using deprecated Django APIs

--- pretix_hobex/__init__.py
+++ pretix_hobex/__init__.py
@@ -1,1 +1,1 @@
-__version__ = '1.0.0'
+from pretix_oppwa import __version__
--- pretix_oppwa/apps.py
+++ pretix_oppwa/apps.py
@@ -1,11 +1,10 @@
 from django.utils.translation import gettext_lazy
+from pretix_oppwa import __version__
 
 try:
     from pretix.base.plugins import PluginConfig
 except ImportError:
     raise RuntimeError("Please use pretix 2.7 or above to run this plugin!")
-
-__version__ = '1.0.0'
 
 
 class PluginApp(PluginConfig):
